90d7696930c49b61a3ecbca66b91e64e56cadf0c
Banks Humphreys <iobg@Bankss-MacBook-Pro.local>
2016-06-01 14:48:20 -0500

    Replaced oninput with event listeners

--- strings.js
+++ strings.js
@@ -1,3 +1,11 @@
+var input = document.getElementById('input');
+input.addEventListener("keyup", function(){
+	if(event.keyCode <65 || event.keyCode>91){
+		input.value = input.value = input.value.replace(event.keyCode, "");
+	}
+	callFun(input.value);
+})
+
 function reversal(string) {
 	document.getElementById("reversal").innerHTML=string.split("").reverse().join("");
 
@@ -5,7 +13,12 @@ function reversal(string) {
 }
 
 function alphabits(string) {
-	document.getElementById("alphabits").innerHTML=string.split("").sort().join("")
+	document.getElementById("alphabits").innerHTML=string.split("").sort(function(a,b) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    if( a == b) return 0;
+    return a < b ? -1 : 1;
+}).join("");
 
 }
 
